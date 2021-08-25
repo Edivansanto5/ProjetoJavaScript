4b23aec07c52690f0b0bc43db00db43bd3f9e4fb
edivansantos <62451604+vanzim@users.noreply.github.com>
2021-08-25 17:18:51 -0300

    add uma pasta

--- ProjetoFase01/Projeto01/projeto01.js
+++ ProjetoFase01/Projeto01/projeto01.js
@@ -1,5 +1,6 @@
 /* 01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,multiplicação e divisão desses valores.
 */
 console.log('ola')
+console.log('ola')
 
 
